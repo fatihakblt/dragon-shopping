eff646f70862e83a7c7668d80721545f1c360cb0
Fatih akbulut <80004505+fatihakblt@users.noreply.github.com>
2023-08-05 12:18:32 +0300

    Github adresi güncellendi

--- cardcollector.js
+++ cardcollector.js
@@ -16,7 +16,7 @@ async function loadCards()
 
     }
     try {
-        const response = await fetch('http://127.0.0.1:5500/card.json');
+        const response = await fetch('https://fatihakblt.github.io/dragon-shopping/');
         let cardsJson= await response.json();
         //console.log(cardsJson);
         var cards = cardsJson.map(function (card) {
